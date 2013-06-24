7c10acd0d3f55b5bf1406ccc0ee034bf9c643876
Terranz <terranz@terradebian>
2013-06-24 18:26:06 +0400

    fixed jsonp

--- server/spending/src/main/java/ru/terra/spending/web/JsonpCallbackFilter.java
+++ server/spending/src/main/java/ru/terra/spending/web/JsonpCallbackFilter.java
@@ -18,47 +18,38 @@ import org.slf4j.LoggerFactory;
 
 public class JsonpCallbackFilter implements Filter
 {
-	private static final String CALLBACK_KEYWORD = "callback";
-
-	private static Logger log = LoggerFactory.getLogger(JsonpCallbackFilter.class);
-
-	public void init(FilterConfig fConfig) throws ServletException
-	{
-	}
-
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
-	{
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		@SuppressWarnings("unchecked")
-		Map<String, String[]> parms = httpRequest.getParameterMap();
-
-		if (parms.containsKey(CALLBACK_KEYWORD))
-		{
-			String callbackFunction = parms.get(CALLBACK_KEYWORD)[0];
-			log.debug("Wrapping response with JSONP callback '" + callbackFunction + "'");
-			httpResponse.setContentType("text/javascript;charset=UTF-8;;;");
-			GenericResponseWrapper wrapper = new GenericResponseWrapper(httpResponse);
-			chain.doFilter(request, wrapper);
-			OutputStream out = httpResponse.getOutputStream();
-			out.write(callbackFunction.getBytes());
-			out.write("(".getBytes());
-			out.write(wrapper.getData());
-			out.write(");".getBytes());
-			out.flush();
-			out.close();
-			httpResponse.setContentLength(3000 + callbackFunction.length() + wrapper.getContentLength());
-			httpResponse.setHeader("Content-Length", "" + (300 + callbackFunction.length() + wrapper.getContentLength()));
-			log.debug("JsonpCallbackFiler done");
-		}
-		else
-		{
-			chain.doFilter(request, response);
-		}
-
-	}
-
-	public void destroy()
-	{
-	}
+    private static final Logger log = LoggerFactory.getLogger(JsonpCallbackFilter.class);
+    
+    public void init(FilterConfig fConfig) throws ServletException {}
+ 
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+ 
+        @SuppressWarnings("unchecked")
+        Map<String, String[]> parms = httpRequest.getParameterMap();
+ 
+        if(parms.containsKey("callback")) {
+            if(log.isDebugEnabled())
+                log.debug("Wrapping response with JSONP callback '" + parms.get("callback")[0] + "'");
+ 
+            OutputStream out = httpResponse.getOutputStream();
+ 
+            GenericResponseWrapper wrapper = new GenericResponseWrapper(httpResponse);
+ 
+            chain.doFilter(request, wrapper);
+ 
+            out.write(new String(parms.get("callback")[0] + "(").getBytes());
+            out.write(wrapper.getData());
+            out.write(new String(");").getBytes());
+ 
+            wrapper.setContentType("text/javascript;charset=UTF-8");
+ 
+            out.close();
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+ 
+    public void destroy() {}
 }
